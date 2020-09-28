8dec65fd2d9b23e28cd809720e71045460fc839c
Tommy <tommysutrisno88@gmail.com>
2020-09-28 17:57:04 +0700

    add employee database table

---
+++ database/migrations/2020_09_28_104556_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('employee_id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('dob');
+            $table->char('gender',1);
+            $table->datetime('joining_date');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
